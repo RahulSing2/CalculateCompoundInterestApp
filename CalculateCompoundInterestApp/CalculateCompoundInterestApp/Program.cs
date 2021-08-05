dff1ebdd78f7ebdecf797c4e432881d2528890a4
RahulSing2 <88368239+RahulSing2@users.noreply.github.com>
2021-08-05 22:57:20 +0530

    CalculateCompountInterest

---
+++ CalculateCompoundInterestApp/CalculateCompoundInterestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CompoundInterestLibrary;
+
+namespace CalculateCompoundInterestApp
+{   //C# program to calculate the compound interest.
+    class Program
+    {
+        public static void  Main(string[] args)
+        {   
+            int years = 0;
+            int annualCompound = 0;
+            double roi = 0;
+            double amount = 0;
+            Console.WriteLine(DateTime.Now);  //Using a NuGet Package
+            Console.Write("Enter the amount : ");
+            amount = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the rate of interest : ");
+            roi = double.Parse(Console.ReadLine()); //roi is rate of interest
+
+            Console.Write("Enter the total number of years : ");
+            years = int.Parse(Console.ReadLine());
+
+            Console.Write("Compounding frequency : ");
+            annualCompound = int.Parse(Console.ReadLine());
+            CompoundLibrary obj = new CompoundLibrary();
+            obj.CalculateCompoundInterest(amount, roi, years, annualCompound);
+            
+        }
+
+    }
+}
+
---
+++ CalculateCompoundInterestApp/CompoundInterestLibary/CompoundLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompoundInterestLibrary
+{
+   public  class  CompoundLibrary
+    {  
+        public void CalculateCompoundInterest(double amount, double roi, int years, int annualCompound)
+        {
+            double result = 0;
+            int loop = 0;
+            if ((amount > 100 && roi > 0) && (years >0))
+            {
+                for (loop = 1; loop <= years; loop++)
+                {
+                    result = amount * Math.Pow((1 + roi / annualCompound), (annualCompound * loop));
+                    Console.WriteLine("Your amount after {0} Year " + ": {1}", loop, result);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Amount should be greater than 100 and Rate of interest and years should be greater than 1");
+            }
+        }
+   }
+}
+
